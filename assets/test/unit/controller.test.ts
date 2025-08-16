c9f1c8a7607807c9daedd08d2d5424e249ecd2d9
Hugo Alliaume <hugo@alliau.me>
2025-08-16 19:17:43 +0200

    Configure Vitest for unit and browser tests (use @puppeteer/browsers and webdriverio)

--- assets/test/unit/controller.test.ts
+++ assets/test/unit/controller.test.ts
@@ -11,8 +11,8 @@ import { Application, Controller } from '@hotwired/stimulus';
 import { getByTestId, waitFor } from '@testing-library/dom';
 import user from '@testing-library/user-event';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
-import { clearDOM, mountDOM } from '../../../../test/stimulus-helpers';
-import DropzoneController from '../src/controller';
+import { clearDOM, mountDOM } from '../../../../../test/stimulus-helpers';
+import DropzoneController from '../../src/controller';
 
 // Controller used to check the actual controller was properly booted
 class CheckController extends Controller {
